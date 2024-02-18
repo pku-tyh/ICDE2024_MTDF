2e045bc0ed33ef44baf814cb2c363aef2f996b42
Tang Yuhao <35452821+pku-tyh@users.noreply.github.com>
2024-02-18 20:09:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ for SOURCE_INDEX in 0 1 2 3; do
                 --epochs 100 --number_of_run $NUM_RUNS --use_bn 1 --batch_size 1024 --lr 0.001 --sdfa_lr 0.001\
                 --hidden-dim 128 --cross_dataset 0 --conv_type GCN\
                 --data_split 4 --source_index $SOURCE_INDEX --target_index $TARGET_INDEX \
-                --eval_interval 5 --target_epochs 30 --sdfa_eval_interval 1
+                --eval_interval 5 --target_epochs 30 --sdfa_eval_interval 1 \
+                --mixup_weight_target 0.3 --mixup_weight_source 0.9 
         fi
     done
 done
